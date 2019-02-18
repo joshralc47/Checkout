e5f53b9939dacd2dc1c13cf5665781cd65d73de5
Josh.Ralston <Josh.Ralston@CF247-DEV-L252>
2019-02-18 14:45:50 +0000

    Added Unit Tests based on recruitments

---
+++ CheckoutKata/CheckoutKataUnitTests/CheckoutUnitTests.cs
@@ -0,0 +1,51 @@
+using System;
+using Xunit;
+
+namespace CheckoutKataUnitTests
+{
+    public class CheckoutUnitTests
+    {
+        [Fact]
+        public void Total_ScanNothing_Return0()
+        {
+
+        }
+
+        [Fact]
+        public void Total_ScanA99_Return50()
+        {
+
+        }
+
+        [Fact]
+        public void Total_ScanB15_Return30()
+        {
+
+        }
+
+        [Fact]
+        public void Total_ScanC40_Return60()
+        {
+
+        }
+
+        [Fact]
+        public void Total_ScanT34_Return99()
+        {
+
+        }
+
+        [Fact]
+        public void Total_ScanThreeA99_Return130()
+        {
+
+        }
+
+        [Fact]
+        public void Total_ScanTwoB15_Return45()
+        {
+
+        }
+
+    }
+}
