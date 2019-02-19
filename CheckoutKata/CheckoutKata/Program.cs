596236214a2b5292cb6ab88329cbe3dfe6e7e6a4
Josh.Ralston <Josh.Ralston@CF247-DEV-L252>
2019-02-19 18:49:28 +0000

    Put some implementation in the program.cs

--- CheckoutKata/CheckoutKata/Program.cs
+++ CheckoutKata/CheckoutKata/Program.cs
@@ -6,7 +6,21 @@ namespace CheckoutKata
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ICheckout checkout = new Checkout(new Store());
+
+            var input = "";
+            Console.WriteLine("Welcome to the checkout please scan your item");
+
+            while(input != "exit")
+            {
+                input = Console.ReadLine();
+                checkout.Scan(input);
+
+                Console.WriteLine($"Shopping basket is now {checkout.Total()}");
+                Console.WriteLine("Scan next item");
+            }
+
+            
         }
     }
 }
